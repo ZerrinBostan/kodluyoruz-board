3c29b03344621a3a8c8eb7ac06581ca529479bc9
ZerrinBostan <46298014+ZerrinBostan@users.noreply.github.com>
2020-07-19 20:34:04 +0300

    ListDropdown was edited.

--- src/components/List/ListDropdown.js
+++ src/components/List/ListDropdown.js
@@ -1,106 +1,72 @@
-import React, { useState, useContext } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Dropdown from '../Dropdown';
 import Icon from '../Icon';
-import listContext from '../../stores/listContext';
 
-const ListDropdown = () => {
-  const { list, setList } = useContext(listContext);
-
-  const [isShow, setShow] = useState(true);
-  const [text, setText] = useState('');
-  const [degree, setDegree] = useState(0);
-  const [description, setDescription] = useState('');
-
-  const handleOnClick = (e) => {
-    const listItem = {
-      text,
-      degree,
-      description,
-    };
-
-    const tempList = [...list, listItem];
-    setList(tempList);
-  };
-
-  const handleToogleDropdown = () => {
-    setShow(!isShow);
-  };
-
-  const handleChangeText = (e) => {
-    setText(e.target.value);
-  };
-
-  const handleChangeDegree = (e) => {
-    setDegree(e.target.value);
-  };
-
-  const handleChangeDescription = (e) => {
-    setDescription(e.target.value);
-  };
-
-  return (
-    <div className="list-dropdown">
-      <div
-        type="button"
-        className="list-dropdown__button"
-        onClick={handleToogleDropdown}
-      >
-        <span>Liste Oluştur</span>
-        <Icon icon="plus" className="icon icon-plus" size={16} />
-      </div>
-
-      {!isShow && (
-        <div className="list-dropdown__content">
+const ListDropdown = ({ isShow, options, columnDropdown }) => (
+  <>
+    {!isShow && (
+      <div className="list-dropdown__content">
+        {!columnDropdown ? (
+          <>
+            <input
+              type="text"
+              name="title"
+              placeholder="Lütfen Başlık oluşturunuz."
+              className="list-dropdown__context--input"
+            />
+            <div className="list-dropdown__degree">
+              <input
+                type="number"
+                name="degree"
+                placeholder="Önemlilik derecesini giriniz."
+                className="list-dropdown__degree--input"
+              />
+            </div>
+            <div className="list-dropdown__textarea">
+              <textarea
+                name="description"
+                rows="4"
+                cols="50"
+                className="list-add-content__description"
+                placeholder="Açıklamanızı giriniz."
+              />
+            </div>
+            <Dropdown options={options} />
+          </>
+        ) : (
           <input
             type="text"
             name="title"
-            placeholder="Lütfen Başlık oluşturunuz."
+            placeholder="Lütfen Kategori adını giriniz."
             className="list-dropdown__context--input"
-            value={text}
-            onChange={handleChangeText}
           />
-          <div className="list-dropdown__degree">
-            <span className="list-dropdown__degree--text">
-              Önemlilik derecesini giriniz.
-            </span>
-            <input
-              type="number"
-              name="degree"
-              placeholder="0"
-              className="list-dropdown__degree--input"
-              value={degree}
-              onChange={handleChangeDegree}
+        )}
+        <div className="list-dropdown__confirmation-content">
+          <div type="button" className="list-dropdown__confirmation" onClick>
+            <span>{columnDropdown ? 'Kategori Oluştur' : 'Liste Oluştur'}</span>
+            <Icon
+              icon="chevrons-down"
+              className="icon icon-chevrons-down"
+              size={16}
             />
-          </div>
-          <div className="list-dropdown__textarea">
-            <span className="list-dropdown__degree--text">
-              Açıklamanızı giriniz.
-            </span>
-            <textarea
-              name="description"
-              rows="4"
-              cols="50"
-              className="list-add-content__description"
-              onChange={handleChangeDescription}
-            />
-          </div>
-          <div className="list-dropdown__confirmation-content">
-            <div
-              type="button"
-              className="list-dropdown__confirmation"
-              onClick={handleOnClick}
-            >
-              <span>Liste Oluştur</span>
-              <Icon
-                icon="chevrons-down"
-                className="icon icon-chevrons-down"
-                size={16}
-              />
-            </div>
           </div>
         </div>
-      )}
-    </div>
-  );
+      </div>
+    )}
+  </>
+);
+
+ListDropdown.defaultProps = {
+  isShow: false,
+  options: null,
+  columnDropdown: false,
+};
+
+ListDropdown.propTypes = {
+  isShow: PropTypes.bool,
+  options: PropTypes.arrayOf({}),
+  columnDropdown: PropTypes.bool,
 };
 
 export default ListDropdown;
