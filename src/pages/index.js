290ba125267fa7c3df1afde56d10c1bc290b7eea
ZerrinBostan <46298014+ZerrinBostan@users.noreply.github.com>
2020-07-19 20:36:15 +0300

    pages\index.js page was edited.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,63 +1,87 @@
-import React, { useContext } from 'react';
+import React, { useState } from 'react';
+
 import ListDropdown from '../components/List/ListDropdown';
-import ListAddItem from '../components/List/ListAddItem';
-import listContext from '../stores/listContext';
+import ListItem from '../components/List/ListItem';
+import columnData from '../components/temp-column.json';
+import optionData from '../components/temp-todo-category.json';
 
 const Home = () => {
-  const { list } = useContext(listContext);
+  const [columnItem, setColumnItem] = useState(columnData);
+  const [listDropdownShow, setListDropdownShow] = useState(true);
+  const [columnDropdownShow, setColumnDropdownShow] = useState(true);
+
+  const setList = (columnData) => {
+    setColumnItem(columnData);
+  };
+
+  const handleToogleDropdown = () => {
+    setListDropdownShow(!listDropdownShow);
+  };
+
+  const handleColumnToogleDropdown = () => {
+    setColumnDropdownShow(!columnDropdownShow);
+  };
+
   return (
-    <div className="board-wrapper">
-      <div className="board-wrapper__item">
-        {list.length === 0 && <ListDropdown />}
-        {list.map((item) => (
-          <ListAddItem
-            text={item.text}
-            degree={item.degree}
-            description={item.description}
+    <>
+      <navbar className="navbar">
+        <span className="navbar--span">TODO BOARD</span>
+        <div className="buttons-wrapper">
+          <button
+            type="button"
+            className={`action-button ${!columnDropdownShow && 'active'}`}
+            onClick={handleColumnToogleDropdown}
+          >
+            sütun ekle
+          </button>
+          <ListDropdown
+            setList={setList}
+            isShow={columnDropdownShow}
+            columnDropdown
           />
+          <button
+            type="button"
+            className={`action-button ${!listDropdownShow && 'active'}`}
+            onClick={handleToogleDropdown}
+          >
+            kart ekle
+          </button>
+          <ListDropdown
+            setList={setList}
+            isShow={listDropdownShow}
+            options={optionData}
+          />
+        </div>
+      </navbar>
+      <div className="board-wrapper">
+        {columnItem.map((columItem) => (
+          <div className="board-wrapper__item" key={columItem.id}>
+            <span
+              className="board-wrapper__item--text"
+              key={columItem.taskItem?.taskId}
+            >
+              {columItem?.title}
+            </span>
+            {columItem.task.map((taskItem) => (
+              <>
+                <ListItem
+                  data={taskItem}
+                  key={taskItem.taskId}
+                  userName={columItem.userName}
+                />
+              </>
+            ))}
+            <button
+              type="button"
+              className={`action-button action-button-column ${!listDropdownShow && 'active'}`}
+              onClick={handleToogleDropdown}
+            >
+              kart ekle
+            </button>
+          </div>
         ))}
       </div>
-      <div className="board-wrapper__item">
-        {list.length === 0 && <ListDropdown />}
-        {list.map((item) => (
-          <ListAddItem
-            text={item.text}
-            degree={item.degree}
-            description={item.description}
-          />
-        ))}
-      </div>
-      <div className="board-wrapper__item">
-        {list.length === 0 && <ListDropdown />}
-        {list.map((item) => (
-          <ListAddItem
-            text={item.text}
-            degree={item.degree}
-            description={item.description}
-          />
-        ))}
-      </div>
-      <div className="board-wrapper__item">
-        {list.length === 0 && <ListDropdown />}
-        {list.map((item) => (
-          <ListAddItem
-            text={item.text}
-            degree={item.degree}
-            description={item.description}
-          />
-        ))}
-      </div>
-      <div className="board-wrapper__item">
-        {list.length === 0 && <ListDropdown />}
-        {list.map((item) => (
-          <ListAddItem
-            text={item.text}
-            degree={item.degree}
-            description={item.description}
-          />
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
 
